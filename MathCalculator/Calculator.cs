dd4a3735420fb3a0bd5892503335e5d79bcadfd0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-05-17 13:01:27 +0530

    ex

--- MathCalculator/Calculator.cs
+++ MathCalculator/Calculator.cs
@@ -1,10 +1,71 @@
 ﻿namespace SimpleCalculatorApp
 {
+    /// <summary>
+    /// Calculator will be used for calculating mathematical results
+    /// </summary>
     public class Calculator // SRP BL
     {
+        /// <summary>
+        /// Find the sum of two non zero positive even numbers sum
+        /// </summary>
+        /// <param name="fno"></param>
+        /// <param name="sno"></param>
+        /// <returns></returns>
+        /// <exception cref="ZeroNumberException"></exception>
+        /// <exception cref="NegativeNumberException"></exception>
+        /// <exception cref="NonEvenNumberException"></exception>
         public int FindSum(int fno, int sno) // BL - SRP
         {
+
+            // only non zero positive even numbers
+
+            /*
+             * Multi
+             * line
+             comments
+             */
+            if (fno == 0 || sno == 0) // zero 
+            {
+                // throws zeronumberexpection when user provides zero values
+                throw new ZeroNumberException("Enter non zero number only");
+            }
+
+            if (fno < 0 || sno < 0) // negative
+            {
+                throw new NegativeNumberException("Enter only non negative numbers only");
+            }
+
+            if (fno % 2 != 0 || sno % 2 != 0) // not even
+            {
+                throw new NonEvenNumberException("Enter only even numbers");
+            }
+
+
             return fno + sno;
+        }
+    }
+
+
+    public class NegativeNumberException : ApplicationException
+    {
+        public NegativeNumberException(string msg) : base(msg)
+        {
+
+        }
+    }
+    public class ZeroNumberException : ApplicationException
+    {
+        public ZeroNumberException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+    public class NonEvenNumberException : ApplicationException
+    {
+        public NonEvenNumberException(string msg) : base(msg)
+        {
+
         }
     }
 }
--- SimpleCalculatorApp/Program.cs
+++ SimpleCalculatorApp/Program.cs
@@ -6,22 +6,57 @@
         {
             // Accept two ints, find the sum them display the result
             //declarations
-            int fno;
-            int sno;
-            int sum = 0;
-            // UI Logic
-            Console.Write("Enter first number: ");
-            fno = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    int fno;
+                    int sno;
+                    int sum = 0;
+                    // UI Logic
+                    Console.Write("Enter first number: ");
+                    fno = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter second number: ");
-            sno = int.Parse(Console.ReadLine());
+                    Console.Write("Enter second number: ");
+                    sno = int.Parse(Console.ReadLine());
 
-            // calculate - BL
-            Calculator calc = new Calculator();
-            sum = calc.FindSum(fno, sno);
+                    // open db connection
 
-            // UI Logic
-            Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+                    // read
+                    // write
+
+                    // calculate - BL
+                    Calculator calc = new Calculator();
+                    sum = calc.FindSum(fno, sno);
+
+                    // UI Logic
+                    Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter only integer number");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Please enter small integer numbers only");
+                }
+
+                catch (NonEvenNumberException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex) // Catch All Block
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    //Console.WriteLine("Finally Block");
+                    // close db
+                }
+            }
 
         }
 
